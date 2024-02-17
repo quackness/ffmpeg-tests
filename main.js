89752a2128a5627e317469f2fd6f4e027d12eaba
Karolina Redden <dubajkaro@gmail.com>
2024-02-17 15:10:26 -0400

    bring video file

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 const ffmpeg = require('fluent-ffmpeg');//functions to perform conversions
-const ffmpeg_static = require('ffmpeg-static');
-const ffprobe_static = require('ffprobe-static');
+const ffmpeg_static = require('ffmpeg-static');//static install of ffmpeg
+const ffprobe_static = require('ffprobe-static');//same for probe, they compliment each other
+
+//load target video files
+const targetVideoFile = __dirname + '/sample-10s.mp4';
+console.log(targetVideoFile);
+
+//ask ffmpeg to convert video file to diffrent format
+
 
